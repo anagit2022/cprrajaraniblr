316df753aea2e825275bf9933c03bb6aed2198c6
anagit2022 <107537622+anagit2022@users.noreply.github.com>
2025-12-09 09:55:24 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -310,6 +310,7 @@ window.onload = () => {
       
         begin1.style.display = "none";
         gender.style.display = "flex";
+        gasp_aud.play();
     };
     beginBtn.onclick = handleBegin;
     beginBtn.addEventListener('touchstart', handleBegin);
