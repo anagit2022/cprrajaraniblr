a784d8e65b620f221f11425dec83eea76afa3504
anagit2022 <107537622+anagit2022@users.noreply.github.com>
2025-12-09 20:39:28 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -700,11 +700,11 @@ window.onload = () => {
                         t6 = setTimeout(() => {
                             cpr4.style.display = "none";
                             cpr5.style.display = "flex";
-                            },5000);
-                        }, 5000);
-                    }, 5000);
-                }, 5000);
-            }, 4000);
+                            },8000);
+                        }, 8000);
+                    }, 8000);
+                }, 8000);
+            }, 8000);
         }, 10000);
     };
     speakerbtn.onclick = handleSpeaker;
